fcb0a62af1eaf1bb22b02f343cf92c99863849f0
유준혁 <under15593@naver.com>
2024-06-05 17:05:04 +0900

    feat: 현재 위치 설정 - 자바스크립트 파일화 1

--- JSFile/get_maps.js
+++ JSFile/get_maps.js
@@ -5,17 +5,13 @@ function getCurrentLocation() {
         .then(userLocation => {
             const lat = userLocation.lat; // get_current_location.js에서 받은 위도
             const lng = userLocation.lng; // get_current_location.js에서 받은 경도
-
-            // 사용자의 현재 위치를 중심으로 지도를 생성
-            const container = document.getElementById('map');
-            const options = {
-                center: new kakao.maps.LatLng(lat, lng), // 현재 위치를 중심으로 설정
-                level: 3 // 확대 수준 설정
-            };
-            const map = new kakao.maps.Map(container, options); //지도 생성 
+            var moveLatLon = new kakao.maps.LatLng(lat, lng);
+        
+            // 지도 중심을 이동 시킵니다
+            map.setCenter(moveLatLon);
         })
         .catch(error => {
             alert(error);
         });
-
 }
+
