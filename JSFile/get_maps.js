5cfda476675540466e4c24935daea21a0d4842ef
유준혁 <under15593@naver.com>
2024-06-05 16:57:19 +0900

    feat: 현재 위치 설정 - 자바스크립트 파일화중

--- JSFile/get_maps.js
+++ JSFile/get_maps.js
@@ -1,9 +1,10 @@
 // 사용자의 현재 위치를 가져와 지도에 표시하는 함수
+
 function getCurrentLocation() {
     handleUserLocation()
         .then(userLocation => {
-            const lat = userLocation.lat(); // get_current_location.js에서 받은 위도
-            const lng = userLocation.lng(); // get_current_location.js에서 받은 경도
+            const lat = userLocation.lat; // get_current_location.js에서 받은 위도
+            const lng = userLocation.lng; // get_current_location.js에서 받은 경도
 
             // 사용자의 현재 위치를 중심으로 지도를 생성
             const container = document.getElementById('map');
