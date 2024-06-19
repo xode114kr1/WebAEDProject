5ef88b82db1c6035072c33715841b17ca6672b19
유준혁 <under15593@naver.com>
2024-06-19 16:57:36 +0900

    Feat: js파일 - 주석 달기

--- JSFile/initiallize_Map.js
+++ JSFile/initiallize_Map.js
@@ -12,18 +12,17 @@ function initializeMap() {
         center: new kakao.maps.LatLng(35.179816, 129.075075),
         level: 3
     };
-    map = new kakao.maps.Map(container, options);
+    map = new kakao.maps.Map(container, options); //카카오맵 출력
 
-    currentLocation()
+    currentLocation() // get__current_location.js파일의 currentLocation()함수 가져오기
         .then(userPosition => {
-            userPosition = new kakao.maps.LatLng(userPosition.lat, userPosition.lng);
-            map.setCenter(userPosition);
+            userPosition = new kakao.maps.LatLng(userPosition.lat, userPosition.lng); // 카카오 api 전역변수 설정
+            map.setCenter(userPosition); // 중심점 userPosition으로 이동
             var imageSrc = "../media/myLocaMarker.png",
                 imageSize = new kakao.maps.Size(55, 46),
-                imageOption = { offset: new kakao.maps.Point(27.5, 46) };
+                imageOption = { offset: new kakao.maps.Point(27.5, 46) };//이미지 속성 정의
             const markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize, imageOption);
-            var userMarker = new kakao.maps.Marker({ position: userPosition, image: markerImage });
-            userMarker.setMap(map);
+            var userMarker = new kakao.maps.Marker({ position: userPosition, image: markerImage }); // 이미지 속성을 지닌 지역변수            userMarker.setMap(map); //
 
             fetch(xmlDataUrl)
                 .then(response => response.text())
@@ -32,18 +31,19 @@ function initializeMap() {
                     const xmlDoc = parser.parseFromString(data, 'text/xml');
                     const items = xmlDoc.getElementsByTagName('item');
 
-                    const radius = 1000;
+                    const radius = 300; //반경 300m이내로 찾기
 
                     for (let i = 0; i < items.length; i++) {
                         const addrs = items[i].getElementsByTagName('addrs')[0].textContent;
                         const geom = items[i].getElementsByTagName('geom')[0].textContent;
                         const [longitude, latitude] = geom.replace('POINT(', '').replace(')', '').split(' ');
                         const tel = items[i].getElementsByTagName('biz_tel')[0].textContent;
-
+                        //공공데이터 api의 데이터 불러오기
                         const markerPosition = new kakao.maps.LatLng(parseFloat(latitude), parseFloat(longitude));
+                        //내 위치와 AED 마커까지의 거리 계산
                         const distance = getDistance(userPosition, markerPosition);
-
-                        if (distance <= radius) {
+                        
+                        if (distance <= radius) { // 반경이내에 있는 AED 마커들을 closetAEDs 배열에 넣기
                             closestAEDs.push({
                                 position: markerPosition,
                                 address: addrs,
@@ -52,25 +52,27 @@ function initializeMap() {
                             })
                         }
                     }
-                    if(closestAEDs.length == 0){
+                    if(closestAEDs.length == 0){ // 만약 반경에 AED없을 때, Modal창 띄우기
                         openModal();
                     }
                     else{
+                        // 거리 순으로 정렬 한 후, 3개 이외에는 자르기
                         closestAEDs.sort((a, b) => a.distance - b.distance);
                         closestAEDs = closestAEDs.slice(0, 3);
-
+                        
                         closestAEDs.forEach(aed => {
                             var imageSrc = "../media/AEDimage.png",
                                 imageSize = new kakao.maps.Size(74, 62),
                                 imageOption = { offset: new kakao.maps.Point(37, 62) };
                             const markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize, imageOption);
-
+                            //마커 이미지 설정
                             const aedMarker = new kakao.maps.Marker({
                                 position: aed.position,
                                 image: markerImage
                             });
-                            aedMarker.setMap(map);
+                            aedMarker.setMap(map); // 지도에 마커 띄우기
 
+                            //AED마커 클릭 시, 이벤트 설정
                             kakao.maps.event.addListener(aedMarker, 'click', function () {
                                 map.panTo(aedMarker.getPosition());
                                 if (customOverlay) {
@@ -85,7 +87,7 @@ function initializeMap() {
                                         <button onclick="toggleWalkingRoute(${userPosition.getLng()}, ${userPosition.getLat()}, ${aed.position.getLng()}, ${aed.position.getLat()})">길찾기</button>
                                     </div>
                                 `;
-
+                                //커스텀 오버레이 속성 설정
                                 customOverlay = new kakao.maps.CustomOverlay({
                                     content: content,
                                     map: map,
@@ -109,6 +111,7 @@ function initializeMap() {
 
 window.onload = initializeMap;
 
+//커스텀 오버레이 닫기 버튼
 function closeOverlay() {
     if (customOverlay) {
         customOverlay.setMap(null);
@@ -152,6 +155,7 @@ function drawWalkingRoute(startX, startY, endX, endY) {
 
         },
         success: function (response) {
+            //Tmap API를 활용해 도보 길찾기 기능 가져오기
             var resultData = response.features;
             var points = [];
             var totalDistance = 0;
@@ -162,7 +166,6 @@ function drawWalkingRoute(startX, startY, endX, endY) {
             for (var i in resultData) {
                 var geometry = resultData[i].geometry;
                 var properties = resultData[i].properties;
-
                 if (geometry.type == "LineString") {
                     for (var j in geometry.coordinates) {
                         var point = new Tmapv2.Point(geometry.coordinates[j][0], geometry.coordinates[j][1]);
@@ -177,6 +180,7 @@ function drawWalkingRoute(startX, startY, endX, endY) {
                     totalTimeInSeconds += properties.totalTime ? properties.totalTime : 0;
                 }
             }
+            // 주소와 전화번호는 해당 함수 밖에 있기에 closetAEDs.find()를 통해 가져옴
             var aedInfo = closestAEDs.find(aed => aed.position.getLng() === endX && aed.position.getLat() === endY);
             if (aedInfo) {
                 address = aedInfo.address;
@@ -198,7 +202,7 @@ function drawWalkingRoute(startX, startY, endX, endY) {
         }
     });
 }
-//지도에 경로 있는 경우엔 버튼 다시 누르면 삭제. 없는 경우엔 그리기
+//지도에 경로 그리기
 function drawLineOnMap(points) {
     currentPolyline = new kakao.maps.Polyline({
         path: points,
@@ -213,7 +217,7 @@ function drawLineOnMap(points) {
 function toggleWalkingRoute(startX, startY, endX, endY) {
     var avgX = (startX + endX) / 2;
     var avgY = (startY + endY) / 2;
-    const avgPosition = new kakao.maps.LatLng(avgY, avgX);
+    const avgPosition = new kakao.maps.LatLng(avgY, avgX); // 내 위치와 AED마커 위치의 중심으로 지도를 옮기기
     map.panTo(avgPosition);
     if (currentPolyline) {
         currentPolyline.setMap(null);
@@ -226,6 +230,7 @@ function toggleWalkingRoute(startX, startY, endX, endY) {
     }
 }
 
+// 길찾기 버튼을 눌렀을 경우, 밑에 창으로 해당 정보들을 전달
 function updateRouteInfo(address, tel, distance, time) {
     const routeInfo = document.getElementById('routeInfo');
     routeInfo.innerHTML = `
@@ -234,11 +239,10 @@ function updateRouteInfo(address, tel, distance, time) {
         <p>왕복 거리: ${(distance / 1000).toFixed(2)} km</p>
         <p>왕복 소요 시간: ${time}</p>
     `;
-   // document.getElementById('bottomBar').style.display = 'block';
 }
 
 
-// Modal handling
+// 주변 AED 마커가 없을 때, 뒤로가기 버튼이 포함된 모달 생성하는 함수
 function openModal() {
     document.getElementById('myModal').style.display = 'block';
 }
